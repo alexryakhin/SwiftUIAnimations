369af3c1947c1efd46b3e703c57e68f8f8fd735f
Alexander Riakhin <bonney977@gmail.com>
2023-12-18 06:33:49 +0100

    Initial Commit

---
+++ SwiftUIAnimations/SwiftUIAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationsApp.swift
+//  SwiftUIAnimations
+//
+//  Created by Aleksandr Riakhin on 12/18/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
